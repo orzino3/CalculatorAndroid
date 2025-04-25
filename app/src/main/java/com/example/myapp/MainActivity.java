d8bbb2e7ad709b475ca2f0dd6e1c9b59d4653a59
Or Zino <orzino3@gmail.com>
2025-04-25 15:27:41 +0300

    update functionality

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -4,7 +4,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -20,7 +25,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
         textView = findViewById(R.id.textViewResult);
     }
 
@@ -93,7 +104,7 @@ public class MainActivity extends AppCompatActivity {
         if (result == (int) result) {
             textView.setText(String.valueOf((int) result));
         } else {
-            textView.setText(String.format("%.2f", result));
+            textView.setText(String.format("%.3f", result));
         }
         num1 = result;
     }
